5871e090fa07caec2f090ef0aa09c92ab92177fb
kimdw9983 <93891414+kimdw9983@users.noreply.github.com>
2023-03-14 10:44:41 +0900

    craco setting

--- craco.config.js
+++ craco.config.js
@@ -4,13 +4,12 @@ module.exports = {
       return {
         ...webpackConfig,
         entry: {
-          main: [env === 'development' &&
-          require.resolve('react-dev-utils/webpackHotDevClient'),paths.appIndexJs].filter(Boolean),
+          main: [env === 'development' && require.resolve('react-dev-utils/webpackHotDevClient'), paths.appIndexJs].filter(Boolean),
           content: './src/scripts/main.tsx',
         },
         output: {
           ...webpackConfig.output,
-          filename: './scripts/main.js',
+          filename: 'static/scripts/[name].js',
         },
         optimization: {
           ...webpackConfig.optimization,
