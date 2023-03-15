16f034fc44f1bbb3fec4a3f7523c2d738115783a
kimdw9983 <93891414+kimdw9983@users.noreply.github.com>
2023-03-15 18:24:42 +0900

    UI test

--- src/scripts/main.tsx
+++ src/scripts/main.tsx
@@ -1,33 +1,22 @@
 import { getElement, getChatgptRoot } from '../utils/elementUtil'
 
-function isPatched() {
-  const bool = !!document.querySelector('chatgpt-textarea')
-  console.log("isPatched", bool)
-  return bool
-}
-
 async function patch() {
-  if (isPatched()) return
-
   const textarea = getElement('div#__next textarea')
   const submit = textarea?.parentNode?.querySelector('button')
   const regenButton = textarea?.parentElement?.parentNode?.querySelector(".btn-neutral") as HTMLButtonElement
   const cppToolbar = regenButton?.parentElement
-  if (!textarea || !submit || !regenButton || !cppToolbar) {
-    console.error('[ChatGPT++] Failed to find elements', textarea, submit, regenButton, cppToolbar, textarea?.parentElement?.parentNode)
-    return
-  }
+  if (!textarea || !submit || !regenButton || !cppToolbar) return
 
   cppToolbar.classList.add('chatgpt-cpp-toolbar')
   cppToolbar.classList.remove('justify-center')
-  regenButton.style.right = '0'
-  regenButton.style.position = 'absolute'
+  // regenButton.style.right = '0'
+  // regenButton.style.position = 'absolute'
   textarea.classList.add("chatgpt-textarea")
   submit.classList.add('chatgpt-submit')
 
-  const element = document.createElement('div')
-  element.innerHTML = "Hello World!"
-  cppToolbar.appendChild(element)
+  // const element = document.createElement('div')
+  // element.innerHTML = "Hello World!"
+  // cppToolbar.appendChild(element)
 }
 
 const root = getChatgptRoot()
