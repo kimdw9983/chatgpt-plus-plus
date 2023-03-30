3f2e6456400753671f6a826e1a370aefcb7b2a1e
kimdw9983 <93891414+kimdw9983@users.noreply.github.com>
2023-03-30 16:11:27 +0900

    refactor

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -5,10 +5,12 @@ interface Props {
   desc?: string | HTMLElement
   onChange: (e: any) => void
   options: Array<{ value: string | number, label: string }>
+  class?: string
 }
 
 export default function Dropdown(props: Props): JSX.Element {
-  const className = "shadow-sm px-4 pr-8 sm:text-sm focus:outline-none border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]"
+  const defaultClass = "shadow-sm px-4 pr-8 sm:text-sm focus:outline-none border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]"
+  const className = `${defaultClass} ${props?.class}`
 
   return (
     <div className="relative block">
@@ -16,7 +18,7 @@ export default function Dropdown(props: Props): JSX.Element {
       <select className={ className } value={ props.value } onChange={ props.onChange }>
         { props.options.map(({ value, label }) => (
           <option key={ value } value={ value }>{ label }</option>
-        ))}
+        )) }
       </select>
     </div>
   )
--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -5,13 +5,16 @@ interface Props {
   min: number
   max: number
   onChange: (e: any) => void
+  class: string
 }
 
 export default function Slider(props: Props): JSX.Element {
+  const defaultClass = "slider appearance-none h-1 w-full rounded-full bg-gray-300 dark:bg-gray-300 outline-none thumb:p-2 thumb:bg-white dark:thumb:bg-gray-500 thumb:shadow-md thumb:appearance-none thumb:outline-none thumb:rounded-full thumb:focus:outline-none thumb:hover:bg-white thumb:hover:shadow-lg transition-all duration-200"
+  const className = `${defaultClass} ${props?.class}`
+
   return (
     <div>
-      <input type="range" min={ props.min } max={ props.max } value={ props.defaultValue } onChange={ props.onChange }
-      className="slider appearance-none h-1 w-full rounded-full bg-gray-300 dark:bg-gray-300 outline-none thumb:p-2 thumb:bg-white dark:thumb:bg-gray-500 thumb:shadow-md thumb:appearance-none thumb:outline-none thumb:rounded-full thumb:focus:outline-none thumb:hover:bg-white thumb:hover:shadow-lg transition-all duration-200"/>
+      <input type="range" min={ props.min } max={ props.max } value={ props.defaultValue } onChange={ props.onChange } className={ className }/>
     </div>
   )
 }
