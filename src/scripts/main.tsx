173a3dc333c8462a9e3c8eb128be360d2c85ec0c
kimdw9983 <93891414+kimdw9983@users.noreply.github.com>
2023-03-31 15:18:43 +0900

    refactor

--- src/scripts/main.tsx
+++ src/scripts/main.tsx
@@ -1,7 +1,7 @@
 import { render } from 'preact'
 import { BooleanProvider } from '../hooks/booleanContext'
 import { getElement, getChatgptRoot } from '../utils/element'
-import { getInputClassName, getToolbarWidth, calculateToolbarPosition, toolbarButtonRight, toolbarButtonWidth } from '../utils/ui'
+import { uiUtils } from '../utils/ui'
 import ToggleButton from '../components/toggleButton'
 import Toolbar from '../components/toolbar'
 
@@ -20,19 +20,18 @@ async function patch() {
   buttonContainer.style.display = 'flex'
   buttonContainer.style.alignItems = 'center'
   buttonContainer.style.position = 'absolute'
-  buttonContainer.style.right = toolbarButtonRight + 'px'
+  buttonContainer.style.right = uiUtils.toolbarButtonRight + 'px'
 
-  const inputContainerClass = inputContainer.className
-  const inputClassName = getInputClassName(inputContainerClass)
-  const toolbarWidth = getToolbarWidth(inputContainer)
-  const toolbarLeft = calculateToolbarPosition(inputContainer)
-  const toolbarButtonContainer = document.createElement('div')
+  const inputClassName = `${uiUtils.getBoxClassName()} absolute py-2 md:pl-4 cpp-toolbar`
+  const toolbarWidth = uiUtils.getToolbarWidth(inputContainer)
+  const toolbarLeft = uiUtils.calculateToolbarPosition(inputContainer)
   const toolbarButton = (
     <BooleanProvider>
       <Toolbar style={{ top: '-275%', width: toolbarWidth, left: toolbarLeft }} className={ inputClassName } />
-      <ToggleButton text={"⚙️"} style={{ width: toolbarButtonWidth+"px", height: "24px", fontSize: "10pt" }} className={ "cpp-toolbarButton" } />
+      <ToggleButton text={"⚙️"} style={{ width: uiUtils.toolbarButtonWidth+"px", height: "24px", fontSize: "10pt" }} className={ "cpp-toolbarButton" } />
     </BooleanProvider>
   )
+  const toolbarButtonContainer = document.createElement('div')
   buttonContainer.appendChild(toolbarButtonContainer)
   render(toolbarButton, toolbarButtonContainer) 
 }
--- src/utils/ui.ts
+++ src/utils/ui.ts
@@ -1,14 +1,20 @@
-export const toolbarButtonRight = 36
-export const toolbarButtonWidth = 28
+export const uiUtils = {
+  toolbarButtonRight: 36,
+  toolbarButtonWidth: 28,
+  getToolbarWidth(inputContainer: HTMLElement): string {
+    return window.getComputedStyle(inputContainer).width
+  },
 
-export function getInputClassName(className: string): string {
-  return className.replace("relative", "absolute").replace(/md:py-3/g, "").replace("flex-grow", "").replace("w-full", "").replace("flex-col", "") + " cpp-toolbar"
-}
+  calculateToolbarPosition(inputContainer: HTMLElement): string {
+    return -Number(this.getToolbarWidth(inputContainer).replace("px", "")) + this.toolbarButtonRight + this.toolbarButtonWidth + "px"
+  },
 
-export function getToolbarWidth(inputContainer: HTMLElement): string {
-  return window.getComputedStyle(inputContainer).width
-}
-
-export function calculateToolbarPosition(inputContainer: HTMLElement): string {
-  return -Number(getToolbarWidth(inputContainer).replace("px", "")) + toolbarButtonRight + toolbarButtonWidth + "px"
+  /**
+   * Returns ChatGPT-themed box element className
+   * 
+   * @returns {string} ChatGPT-themed className
+   */
+  getBoxClassName(): string {
+    return "flex border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]"
+  }
 }
