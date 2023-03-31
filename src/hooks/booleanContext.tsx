747231ce61a177a65a3f12981ba0b70e53f04f7b
kimdw9983 <93891414+kimdw9983@users.noreply.github.com>
2023-03-31 17:34:15 +0900

    add setBool

--- src/hooks/booleanContext.tsx
+++ src/hooks/booleanContext.tsx
@@ -1,24 +1,26 @@
 import { createContext, JSX } from 'preact'
-import { useState, useContext, useMemo } from 'preact/hooks'
+import { useState, useContext, useMemo, StateUpdater } from 'preact/hooks'
 
 export interface BooleanType {
   bool: number
+  setBool: StateUpdater<number>
   toggle: () => void
 }
 const BooleanContext = createContext<BooleanType>({ 
   bool: 0,
+  setBool: () => null,
   toggle: () => null
 })
 
 export const BooleanProvider = ({ children }: { children: JSX.Element | JSX.Element[] }) => {
-  const [bool, setBoolean] = useState<number>(0)
+  const [bool, setBool] = useState<number>(0)
 
   function toggle() {
-    setBoolean(bool ? 0 : 1)
+    setBool(bool ? 0 : 1)
   }
 
   const value = useMemo(() => {
-    return { bool, toggle }
+    return { bool, setBool, toggle }
   }, [bool])
 
   return (
@@ -29,6 +31,6 @@ export const BooleanProvider = ({ children }: { children: JSX.Element | JSX.Elem
 }
 
 export const useBoolean = () => {
-  const { bool, toggle } = useContext(BooleanContext)
-  return { bool, toggle }
+  const { bool, setBool, toggle } = useContext(BooleanContext)
+  return { bool, setBool, toggle }
 }
