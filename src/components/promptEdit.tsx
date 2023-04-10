ab359845dd850a5b765e42a2ab889a9bbce82667
kimdw9983 <93891414+kimdw9983@users.noreply.github.com>
2023-04-10 17:59:02 +0900

    prompt list button

--- src/components/promptEdit.tsx
+++ src/components/promptEdit.tsx
@@ -10,12 +10,15 @@ interface PromptEditProps {
 function PromptBox(props: { prompt: Prompt }) {
   const promptName = props.prompt.name
   const promptBody = props.prompt.body
+  const isDefault = props.prompt.id === "default"
 
   return (
-  <div className="flex">
-    <input type='button' value={ promptName } title={ promptBody } />
-    <input type='button' value="🗑️" title="Delete this prompt" />
-    <input type='button' value={ props.prompt.showOnToolbar ? "✔️" : "🗙"} title="Toggle visibility on toolbar" />
+  <div className="flex flex-col gap-2 text-gray-100 text-sm">
+    <a className="flex py-3 px-3 items-center gap-3 relative rounded-md hover:bg-[#2A2B32] cursor-pointer break-all hover:pr-4 group" title={ promptBody }>
+      { promptName }
+    </a>
+    { !isDefault && <input type='button' value="🗑️" title="Delete this prompt" /> }
+    { !isDefault && <input type='button' value={ props.prompt.showOnToolbar ? "✅" : "🗙"} title="Toggle visibility on toolbar" /> }
   </div>
   )
 }
@@ -26,10 +29,10 @@ function PromptList() {
 
   return (
   <div>
-    <div className="overflow-auto">
-      {Object.entries(promptList).map(([key, prompt]) => {
+    <div className="flex-col flex-1 overflow-y-auto border-b border-white/20 -mr-2">
+      {Object.entries(promptList).map(([key, prompt]) => 
         <PromptBox key={ key } prompt={ prompt } />
-      })}
+      )}
     </div>
   </div>
   )
