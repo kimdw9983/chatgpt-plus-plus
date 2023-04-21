c942bab3342b0aadc3712f97b4ff26deeb89514d
kimdw9983 <93891414+kimdw9983@users.noreply.github.com>
2023-04-21 14:04:25 +0900

    fixed submitting message

--- src/scripts/main.tsx
+++ src/scripts/main.tsx
@@ -7,13 +7,13 @@ import { readPromptSetting } from '../managers/prompt'
 import { readPrompt } from '../managers/prompt'
 import { resolvePattern } from '../managers/prompt'
 
-const toolbarButtonRight = 36
+const toolbarButtonRight = 38
 const toolbarButtonWidth = 28
 function getToolbarWidth(inputContainer: HTMLElement) {
   return window.getComputedStyle(inputContainer).width
 }
 function calculateToolbarPosition(inputContainer: HTMLElement): string {
-  return -Number(getToolbarWidth(inputContainer).replace("px", "")) + toolbarButtonRight + toolbarButtonWidth + "px"
+  return -Number(getToolbarWidth(inputContainer).replace("px", "")) + toolbarButtonWidth + 38 + "px"
 }
 
 async function applyPrompt(textarea: HTMLTextAreaElement) {
@@ -46,8 +46,9 @@ async function patch() {
   const chatgptTextarea = document.querySelector('div#__next textarea') as HTMLTextAreaElement
   const chatgptSubmit = chatgptTextarea?.parentNode?.querySelector('button') as HTMLButtonElement
   const chatgptForm = chatgptTextarea?.parentElement?.parentElement?.parentElement as HTMLFormElement
+  const chatgptFormHolder = chatgptForm?.parentElement as HTMLDivElement
   const inputContainer = chatgptTextarea?.parentElement as HTMLDivElement
-  if (!chatgptTextarea || !chatgptSubmit || !inputContainer || !chatgptForm ) return
+  if (!chatgptTextarea || !chatgptSubmit || !inputContainer || !chatgptForm || !chatgptFormHolder ) return
 
   chatgptTextarea.classList.add("chatgpt-textarea")
   chatgptSubmit.classList.add("chatgpt-submit")
@@ -64,29 +65,29 @@ async function patch() {
     submit(chatgptTextarea)
   }, {capture: true})
 
-  chatgptForm.addEventListener('click', (event: MouseEvent) => {
-    console.log(event)
-    const target = event.target as HTMLElement
-
-    if (target.nodeName === 'BUTTON') { // BUTTON 근처에 것도 클릭할 수 있음.
-      event.preventDefault()
-      event.stopPropagation()
-
-      const buttonId = target.id
-      console.log(`Button with ID "${buttonId}" was clicked.`)
-    }
-  }, {capture: true})
-
   const buttonContainer = document.createElement('div')
-  inputContainer.appendChild(buttonContainer)
-  buttonContainer.classList.value = "flex absolute text-gray-500 bottom-1.5 md:bottom-2.5 items-center"
-  buttonContainer.style.right = toolbarButtonRight + 'px'
+  function positionToolbarButton(buttonContainer: HTMLDivElement, e?:any) {
+    //I had to move this button outside of the form and hack the position related to textarea's size and location. 
+    //Because the event for submitting the message is bound to all descendants of the form, not the direct child, currently.
+    const formRect = chatgptForm.getBoundingClientRect()
+    const buttonXPos = formRect.right - toolbarButtonWidth - 38
+    buttonContainer.style.width = `${toolbarButtonWidth}px`
+    buttonContainer.style.left = `${buttonXPos}px`
+    console.log("resize", formRect, toolbarButtonWidth, buttonXPos)
+  }
+  chatgptFormHolder.appendChild(buttonContainer) 
+  positionToolbarButton(buttonContainer)
+  window.addEventListener("resize", (e) => positionToolbarButton(buttonContainer))
+  
+  buttonContainer.classList.value = "flex fixed text-gray-500 items-center"
+  buttonContainer.style.right = 486 + 'px'
+  buttonContainer.style.bottom = 63 + 'px'
 
   const toolbarWidth = getToolbarWidth(inputContainer)
   const toolbarLeft = calculateToolbarPosition(inputContainer)
   const toolbarButton = (
   <BooleanProvider>
-    <Toolbar style={{ top: '-12px', width: toolbarWidth, left: toolbarLeft, transform: "translate(0, -100%)" }} />
+    <Toolbar style={{ top: '-14px', width: toolbarWidth, left: toolbarLeft, transform: "translate(0, -100%)" }} />
     <ToggleButton innerText={ <svg.settings/> } style={{ width: toolbarButtonWidth+"px", height: "24px", fontSize: "10pt" }} className={ "cpp-toolbar-button" } />
   </BooleanProvider>
   )
